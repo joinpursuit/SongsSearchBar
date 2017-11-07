a01861879c58f8565381e9978578a0bb1705a19c
Marlon Rugama <MarlonRugama@ac.c4q.nyc>
2017-11-07 10:32:05 -0500

    Final SearchBar

--- SongsTableViewSearchBar/ViewController.swift
+++ SongsTableViewSearchBar/ViewController.swift
@@ -8,20 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var songs: [Song] = []
+    var searchText: String? = nil {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
+    var filteredSongs: [Song] {
+        guard let searchText = searchText else {
+            return songs
+        }
+        return songs.filter{$0.name.lowercased().contains(searchText.lowercased())}
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.searchBar.delegate = self
+        songs = Song.loveSongs
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: - Table View Data Source
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredSongs.count
     }
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let song = filteredSongs[indexPath.row]
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "Song Cell", for: indexPath)
+        cell.textLabel?.text = song.name
+        cell.detailTextLabel?.text = song.artist
+        cell.imageView?.image = UIImage(named: "loveSongs")
+        return cell
+    }
+    
+    //MARK: - Search Bar Delegate Methods
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.searchText = searchText
+    }
 
 }
 
