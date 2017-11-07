ff08788e55cc72c2411c6c6784d769fea3dbd7b7
Tyler Zhao <tylerzhao@ac.c4q.nyc>
2017-11-07 10:18:25 -0500

    initial and final commit, assignment completed

---
+++ SongsTableViewSearchBar/SongDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SongDetailViewController.swift
+//  SongsTableViewSearchBar
+//
+//  Created by C4Q on 11/6/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class SongDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
