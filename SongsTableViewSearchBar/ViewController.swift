eb8b43295eadbd5a0c4c487adb9f25c96a775b50
Pursuit <pursuit@Pursuits-MacBook-Pro.local>
2019-08-15 17:18:59 -0400

    Added answers to Songs Table View

--- SongsTableViewSearchBar/ViewController.swift
+++ SongsTableViewSearchBar/ViewController.swift
@@ -8,20 +8,105 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var music = Song.loveSongs
 
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var personSearchResults: [Song] {
+        get {
+           
+            guard let searchString = searchString else{
+                return music
+            }
+            guard searchString != "" else {
+                return music
+            }
+            if let scopeTitles = searchBar.scopeButtonTitles{
+                let currentScopeIndex = searchBar.selectedScopeButtonIndex
+                switch scopeTitles[currentScopeIndex]{
+                    
+                case "Name":
+                    let results = music.filter{$0.name.lowercased().contains(searchString.lowercased())}
+                    
+                    if results.count == 0 {
+                        messageLabel.text = "No results found"
+                        return []
+                    
+                    }else {
+                    messageLabel.text = ""
+                    return music.filter{$0.name.lowercased().contains(searchString.lowercased())}
+                    }
+                
+                case "Artist":
+                    let results = music.filter{$0.artist.lowercased().contains(searchString.lowercased())}
+                    
+                    if results.count == 0 {
+                        messageLabel.text = "No results found"
+                        return []
+                        
+                    }else{
+                        messageLabel.text = ""
+                        return music.filter{$0.artist.lowercased().contains(searchString.lowercased())}
+                    }
+                default:
+                    return music
+                }
+            }
+            return music
+        }
+    }
+    var searchString: String? = nil {
+        didSet{
+            print(searchString)
+            self.songTableView.reloadData()
+        }
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return personSearchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ourCell", for: indexPath)
+        let popSong = personSearchResults[indexPath.row]
+        cell.textLabel?.text = popSong.name
+        cell.detailTextLabel?.text = popSong.artist
+        
+        return cell
+    }
+    
+
+    
+    @IBOutlet weak var songTableView: UITableView!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       songTableView.dataSource = self
+        searchBar.delegate = self
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
     }
 
 
 }
 
+extension ViewController: UISearchBarDelegate {
+    internal func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchString = searchBar.text
+        
+}
+
+}
