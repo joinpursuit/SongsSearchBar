6dd470d680590ae6ac9fccfb7f2769b04bf3a6c5
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-08-15 17:20:47 -0400

    creates searchbar outlet and search string variable

--- SongsTableViewSearchBar/Controllers/ViewController.swift
+++ SongsTableViewSearchBar/Controllers/ViewController.swift
@@ -10,10 +10,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
     let songs = Song.loveSongs
     
-    @IBOutlet weak var tableView: UITableView!
+    
+    var searchString: String? = nil {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
+    
+
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
