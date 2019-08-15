6fa320cfd784ae1c28d7cff1cd106ceaf7913f25
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-08-15 17:03:03 -0400

    adds tableview outlet and methods in VC

--- SongsTableViewSearchBar/Controllers/ViewController.swift
+++ SongsTableViewSearchBar/Controllers/ViewController.swift
@@ -1,1 +1,51 @@
 
+//
+//  ViewController.swift
+//  SongsTableViewSearchBar
+//
+//  Created by C4Q  on 11/6/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+
+
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      return UITableViewCell()
+    }
+    
+}
+
+
+
+
+
+
