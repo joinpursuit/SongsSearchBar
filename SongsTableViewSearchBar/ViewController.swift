b35f672feb5b7933af9ef57798a290726291f239
Kevin Natera <mycityneedsme@Kevins-MacBook-Pro.local>
2019-08-15 15:59:11 -0400

    Populated TableView and Added Searchbar

--- SongsTableViewSearchBar/ViewController.swift
+++ SongsTableViewSearchBar/ViewController.swift
@@ -8,13 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+    
+    var songs = Song.loveSongs
+    var searchString: String? = nil {
+        didSet {
+            self.tableOutlet.reloadData()
+        }
+    }
+//    var songSearchResults: [Song] {
+//        get {
+//            guard let _ = searchString else {
+//                return songs
+//            }
+//            guard searchString != "" else {
+//                return songs
+//            }
+//            if let scopeTitles = sea {
+//                let currentScopeIndex = searchBarOutlet.selectedScopeButtonIndex
+//
+//                switch scopeTitles[currentScopeIndex] {
+//                case "Title":
+//                    return songs.filter {$0.name.contains(searchString!.lowercased()) }
+//                case "Phone":
+//                    return songs.filter { $0.artist.contains(searchString!.lowercased()) }
+//                default:
+//                    return songs
+//                }
+//        }
+//    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableOutlet.dequeueReusableCell(withIdentifier: "Song")
+        
+        cell?.textLabel?.text = songs[indexPath.row].name
+        cell?.detailTextLabel?.text = songs[indexPath.row].artist
+        
+        return cell!
+    }
+    
+
+    
+    @IBOutlet weak var tableOutlet: UITableView!
+    
+    @IBOutlet weak var searchBarOutlet: UISearchBar!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        searchBarOutlet.delegate = self
+        tableOutlet.dataSource = self
+        tableOutlet.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
