259796268e373a8ad8ba62c20ed93335a6750d76
Bienbenido Angeles <bienbenido.angeles@gmail.com>
2019-12-03 02:39:40 -0500

    level 1 complete

--- SongsTableViewSearchBar/ViewController.swift
+++ SongsTableViewSearchBar/ViewController.swift
@@ -9,19 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView:UITableView!
+    
+    var songs = [Song](){
+        didSet{
+            tableView.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadData()
+        tableView.dataSource = self
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dvc = segue.destination as? DetailViewController, let indexPath = tableView.indexPathForSelectedRow else {
+            fatalError("failed to segue to DetailViewController")
+        }
+        let song = songs[indexPath.row]
+        dvc.song = song
     }
-
-
+    
+    func loadData(){
+        songs = Song.loveSongs
+    }
+    
 }
 
+extension ViewController:UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "songCell", for: indexPath)
+        let song = songs[indexPath.row]
+        
+        cell.textLabel?.text = song.name
+        cell.detailTextLabel?.text = song.artist
+        
+        return cell
+    }
+}
