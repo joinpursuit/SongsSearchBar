e265aae8db4250e0742b64aee634608b859bbad3
Ahad Islam <34593133+ahadislam1@users.noreply.github.com>
2019-12-09 15:40:09 -0500

    Displayed love songs onto a tableview.

--- SongsTableViewSearchBar/ViewController.swift
+++ SongsTableViewSearchBar/ViewController.swift
@@ -9,19 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let songs = Song.loveSongs
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Love Song Cell", for: indexPath)
+        
+        cell.textLabel?.text = songs[indexPath.row].name
+        cell.detailTextLabel?.text = songs[indexPath.row].artist
+        
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {}
 
